22f83feccc0a7e6a31483df1138d798763197473
Dwire <gjdwyer8@gmail.com>
2018-04-26 17:08:18 -0400

    add methods

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ let store = {
   employers: []
 }
 
-let Customer = (
+
+let Customer = (() => {
   let customerId = 0;
+
   return class Customer{
 
     constructor(name, employer){
@@ -15,16 +17,23 @@ let Customer = (
       if(employer){
         this.employerId = employer.id
       }
+
+      store.customers.push(this)
     }
 
 
 
     meals(){
-
+      // return store.deliveries.filter((meal) => {
+      //   return meal.customerId === this.id
+      // })
     }
 
     deliveries(){
-
+      debugger
+      return store.deliveries.filter((delivery) => {
+        return delivery.customerId === this.id
+      })
     }
 
     totalSpent(){
@@ -32,9 +41,15 @@ let Customer = (
     }
 
   }
-)()
 
-let Meal = (
+})()
+  // let bob = new Customer("Bob")
+  // let jen = new Customer("Jen")
+  // let dan = new Customer("Dan")
+  //
+
+
+let Meal = (() => {
   let mealId = 0;
   return class Meal{
     constructor(title, price){
@@ -42,12 +57,13 @@ let Meal = (
       this.title = title
       this.price = price
 
+      store.meals.push(this)
     }
 
   }
-)()
+})()
 
-let Delivery = (
+let Delivery = (() => {
   let deliveryId = 0;
   return class Delivery{
     constructor(meal, customer){
@@ -59,20 +75,23 @@ let Delivery = (
       if(customer){
         this.customerId = customer.id
       }
+
+      store.deliveries.push(this)
     }
 
   }
-)()
+})()
 
 
-let Employer = (
+let Employer = (() => {
   let employerId = 0;
   return class Employer{
     constructor(name){
       this.id = ++employerId
       this.name = name
 
+      store.employers.push(this)
     }
   }
 
-)()
+})()
